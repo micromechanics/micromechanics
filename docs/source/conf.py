a25b662964124b5f4bd2be373868db9903a0778f
Velislava <v.yonkova@fz-juelich.de>
2022-01-11 17:24:02 +0100

    project name change from nanoindent to micromechanics

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../../source_code'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'nanoIndent'
+project = 'micromechanics'
 copyright = '2021, VPY'
 author = 'VPY'
 
