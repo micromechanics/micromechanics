16a28e73f2b8847968e8cb931740646c1c9d4532
Velislava <v.yonkova@fz-juelich.de>
2022-02-07 09:01:34 +0100

    Simplify tutorial

--- source_code/nanoindent_tutorial.py
+++ source_code/nanoindent_tutorial.py
@@ -19,7 +19,7 @@ This is a tutorial for using nanoIndent.py while analysing nanoindentation data
 
 How it works:
 
-1. Measure maximum force (Pmax) and depth (hmax), get loading and unloading data through nanoindentation
+1. Measure maximum force (Pmax) and depth (hmax), obtain loading and unloading data through nanoindentation
 2. Calculate:
 
     - S [mN/µm]: Slope of unloading part (stiffness)
@@ -29,6 +29,59 @@ How it works:
     - Er [GPa]: reduced Young’s modulus
     - E [GPa]: Young’s modulus
 3. Plot
+
+Getting started
+===============
+Import necessary libraries::
+    
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from nanoIndent import Indentation, Tip
+
+Introduce tip::
+
+    ourTip = Tip()  #µm/mN
+
+Initialization: Files containing the experimental data are in hdf format::
+
+    fileName = 'FileName.hdf5'
+
+Introduce class Indentation, where nuMat is material’s Poisson’s ratio::
+
+    i = Indentation(fileName, nuMat=0.3, tip=ourTip)
+
+Run through all indentations (Test 1, Test 2, etc) in the hdf file with ``while True`` cycle::
+
+    while True:
+        <body>
+        if len(i.testList)==0:
+            break
+        i.nextTest()
+
+In the ``<body>`` one can analyse the data and create the plots to be viewed like following:
+
+Calculate all the relevent data from the indent: stiffness S [mN/µm], maximum depth hMax [µm], maximum force pMax [mN], reduced Young’s modulus redE [GPa],
+contact area A [µm2], contact depth hc [µm], Young’s modulus E [GPa], hardness H [GPa]::
+                
+    i.analyse()
+
+Plotting:
+
+- ``i.plot`` would plot each test(indent) independantly with the analysis data: slope, hc, power law etc
+- ``plt.plot(i.h, i.p)`` would plot all tests(indents) together. Useful to spot outliers::
+
+    i.plot()
+    plt.plot(i.h, i.p)
+
+Save all values to a dictionary. This can be appended to a user defined dataframe and further information can be added to it accordingly. E.g. the file name in this case::
+
+    meta = i.metaUser
+    meta["file name"]=fileName
+    df = df.append(meta, ignore_index=True)
+
+Show plots::
+
+    plt.show()
 
 Example:
 ========
@@ -52,67 +105,6 @@ This is an example code analysing the data from a Fischer nanoindenter::
                 if len(i.testList)==0:
                     break
                 i.nextTest()
-    print(df)
-    plt.show()
-
-Explanation
-===========
-Import necessary libraries::
-    
-    import matplotlib.pyplot as plt
-    import os
-    import numpy as np
-    import pandas as pd
-    from nanoIndent import Indentation, Tip
-
-Create an empty dataframe before the cycle to store all analyzed data::
-
-    df = pd.DataFrame()
-
-Introduce tip::
-
-    ourTip = Tip()  #µm/mN
-
-Initialization: Start cycle to open and read through all hdf files.
-Navigate through the hdf files using ``startswith("...")`` and ``endswith("...")`` like in the example::
-
-    for fileName in os.listdir('.'):
-        if fileName.endswith('100mN_10s.hdf5'):
-            i = Indentation(fileName, nuMat=0.3, tip=ourTip)
-
-Run through all indentations (Test 1, Test 2, etc) in the hdf file with ``while True`` cycle::
-
-    while True:
-        <body>
-        if len(i.testList)==0:
-            break
-        i.nextTest()
-
-In the ``<body>`` one can analyse the data and create the plots to be viewed like following:
-
-Calculate all the relevent data from the indent: stiffness S [mN/µm], maximum depth hMax [µm], maximum force pMax [mN], reduced Young’s modulus redE [GPa],
-contact area A [µm2], contact depth hc [µm], Young’s modulus E [GPa], hardness H [GPa]::
-                
-    i.analyse()
-
-Plotting:
-
-- ``i.plot`` would plot each test(indent) independantly with the analysis data: slope, hc, power law etc
-- ``plt.plot(i.h, i.p)`` would plot all tests(indents) together. Useful to spot outliers::
-
-
-    i.plot()
-    plt.plot(i.h, i.p)
-
-Save all values and add any additional information that might be of use to a dictionary to append to the dataframe storing all analized data accordingly::
-
-    meta = i.metaUser
-    meta["test name"]=i.testName
-    meta["file name"]=fileName
-    df = df.append(meta, ignore_index=True)
-
-Show the plots, print the dataframe if needed::
-
     print(df)
     plt.show()
 
