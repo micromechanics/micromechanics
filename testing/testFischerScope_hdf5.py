d56f2346bc72049484f52b881f6e9cd69a5b3dd4
Velislava <v.yonkova@fz-juelich.de>
2022-03-04 09:26:56 +0100

    edit testing method hdf5

--- testing/testFischerScope_hdf5.py
+++ testing/testFischerScope_hdf5.py
@@ -5,22 +5,23 @@ import numpy as np
 from nanoIndent import Indentation
 
 class TestStringMethods(unittest.TestCase):
-  def test_main(self):
-    try:
-      ### MAIN ###
-      i = Indentation('examples/FischerScope/Nafion_15_100_5.hdf5')
-      i.analyse()
-      self.assertTrue(np.sum(i.modulus)==0.45085650501100577,'Calculation of h changed')
-      ### END OF MAIN ###
-      print('\n*** DONE WITH VERIFY ***')
+	def test_main(self):
+		try:
+			### MAIN ###
+			i = Indentation('examples/FischerScope/Nafion_15_100_5.hdf5')
+			i.analyse()
+			print(np.sum(i.modulus))
+			self.assertTrue((np.sum(i.modulus)==0.45085650501100577)<1e-12,'Calculation of modulus changed')
+			### END OF MAIN ###
+			print('\n*** DONE WITH VERIFY ***')
 
-    except:
-      print('ERROR OCCURRED IN VERIFY TESTING\n'+ traceback.format_exc() )
-      self.assertTrue(False,'Exception occurred')
-    return
+		except:
+			print('ERROR OCCURRED IN VERIFY TESTING\n'+ traceback.format_exc() )
+			self.assertTrue(False,'Exception occurred')
+		return
 
-  def tearDown(self):
-    return
+	def tearDown(self):
+		return
 
 if __name__ == '__main__':
-  unittest.main()
+	unittest.main()
