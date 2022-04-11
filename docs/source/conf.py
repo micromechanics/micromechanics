ab2ddbfa20ccedea3fcf2a388f193d9929d0c546
Velislava <v.yonkova@fz-juelich.de>
2022-04-11 12:09:33 +0200

    edit path for tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../source_code'))
+sys.path.insert(0, os.path.abspath('../../nanoindentation'))
 
 
 # -- Project information -----------------------------------------------------
