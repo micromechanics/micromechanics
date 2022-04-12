5c7f87c0c9bb19d89a363b2d5183b69e2a420731
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2022-04-12 12:44:38 +0200

    Improved testing of CSM measurements

--- testing/testVerification.py
+++ testing/testVerification.py
@@ -1,6 +1,6 @@
 import traceback
 import unittest
-from nanoindentation import Indentation
+from nanoindentation import Indentation, Tip
 
 class TestStringMethods(unittest.TestCase):
 	def test_verify1(self):
@@ -32,12 +32,15 @@ class TestStringMethods(unittest.TestCase):
 	def test_verify3(self):
 		try:
 			# MAIN
-			i = Indentation('examples/Agilent/FS_Calibration.xls', nuMat=0.18)
-			if hasattr(self, 'hc') and hasattr(self, 'Ac') and hasattr(self, 'modulusRed') and \
-				 hasattr(self, 'modulus') and hasattr(self, 'hardness'):
-				i.verifyReadCalc()
-			else:
-				print("\n\n** ERROR file has not enough data for verification! **\n\n")
+			tip = Tip(shape = [2.4695e+001,3.9577e+002,-1.6132e+001,1.3341e+002,1.0646e+002,'iso'])
+			print("**INFO: Tip-area prefactors have accuracy of 1e-4; hence area accuracy on that order")
+			i = Indentation('examples/Agilent/FS_XP.xls', nuMat=0.18, tip=tip)
+			while True:
+				print('Sheet name:', i.testName)
+				i.verifyReadCalc(plot=False)
+				if len(i.testList)==0:
+					break
+				i.nextTest()
 			# END OF MAIN
 			print('\n*** DONE WITH VERIFY ***')
 		except:
