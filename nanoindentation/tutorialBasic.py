2f9aee047f8874096c03bc075d8a28fe29a55e25
Velislava <v.yonkova@fz-juelich.de>
2022-04-28 16:43:05 +0200

    attempt4

--- nanoindentation/tutorialBasic.py
+++ nanoindentation/tutorialBasic.py
@@ -62,39 +62,6 @@ and add in the end for pythonpath the location of the folder, for example::
 
 Save and close.
 
-Windows
-=======
-
-1. Install git.
-
-Go to http://git-scm.com/download/win and download '64-bit Git for Windows Setup' (mostlikely choice). Install git using:
-
-	under "Adjusting your PATH environment" choose:  Git from the command line and also from 3rd-party software
-
-	under "Choosing the ssh executable" choose:  use OpenSSH
-
-	under "Choosing the HTTPS transport backend" choose:  Use the OpenSSL library
-
-	under "Configuring the line ending conversions" choose:  Checkout Windows-Style, commit Unix-Style line endings
-
-	under "Configuring the terminal emulator to use with Git bash" choose:  Use minTTY
-
-	under "Choose the default behavior of 'git pull'" choose:  Default
-
-	under "Choose credential helper" choose:  Git credential Manager Core
-
-	under "Configure extra options" choose:  Enable filesystem caching and Enable symbolic links
-
-2. Using the command-line tool "CMD.exe":
-
-	- change to your "MyDocuments"
-	- type "git clone git@github.com:micromechanics/main.git"
-
-3. Adopt the "System Variables" or "Environment Variables" (search "systemvariables" in the Windows search bar) and edit the variables "for your account".
-Edit the entry path and add::
-
-	C:\Users*USER*\Documents\Micromechanics
-
 Initialization
 --------------
 Import necessary libraries::
