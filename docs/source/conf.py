f88d9c235bc2e6cf1d83a7096efef0d06034e073
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2023-03-30 15:22:15 +0200

    add Tif documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ copyright = u'2022-{}, PASTA-ELN team'.format(datetime.datetime.now().year)
 author = u'Micromechanics team'
 
 # The full version, including alpha/beta/rc tags
-version = "1.1.7"
+version = "1.1.8"
 release = version
 
 # -- General configuration ---------------------------------------------------
