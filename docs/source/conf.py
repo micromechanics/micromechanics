5d441125654c391f4a4af043278f1d40afb85daa
Dr. Steffen Brinckmann <sbrinckm@gmail.com>
2023-03-30 15:27:12 +0200

    add opencv requirement

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ copyright = u'2022-{}, PASTA-ELN team'.format(datetime.datetime.now().year)
 author = u'Micromechanics team'
 
 # The full version, including alpha/beta/rc tags
-version = "1.1.8"
+version = "1.1.9"
 release = version
 
 # -- General configuration ---------------------------------------------------
